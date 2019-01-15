f2c09329cc10881760fc801da722a71f74201974
Teri <teri.eich@gmail.com>
2019-01-15 21:59:22 +0000

    added solution for exercise_7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,53 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# class ApparelValidator < ActiveModel::Validator
+#   def validate(record)
+#     unless :mens_apparel != true || :womens_apparel != true
+#       record.errors[:mens_apparel, :womens_apparel] << 'at least one of mens_apparel or womens_apparel needs to be true'
+#     end
+#   end
+# end
+
+
+class Employee < ActiveRecord::Base
+  validates :first_name,
+    presence: true
+  validates :last_name,
+    presence: true
+  validates :hourly_rate,
+    numericality: { only_integer: true },
+    inclusion: { in: 40..200 }
+  validates_associated :store
+end
+
+class Store < ActiveRecord::Base
+  # include ActiveModel::Validations
+  # validates_with ApparelValidator
+  validates :name,
+    length: { minimum: 3 }
+  validates :annual_revenue,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :mens_apparel, inclusion: { in: [true, false] }
+  validates :womens_apparel, inclusion: { in: [true, false] }
+  validate :apparel_option_must_be_true
+
+  private
+
+  def apparel_option_must_be_true
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "or womens apparel must be true")
+    end
+  end
+
+end
+
+puts "Please enter a store name >>"
+new_store_name = gets.chomp
+store = Store.create(name: new_store_name, annual_revenue: "", mens_apparel: "", womens_apparel: "")
+store.valid?
+err_arr = store.errors.full_messages
+
+err_arr.each do |msg|
+  print "#{msg}\n"
+end
